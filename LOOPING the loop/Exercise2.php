a927c77b3b0d6156dade678348b46c4e8eb2485a
ITE32b-3@M307.UC-BCF.EDU.PH <ITE32b-3@M307.UC-BCF.EDU.PH>
2018-03-17 15:51:32 +0800

    add more

---
+++ LOOPING the loop/Exercise2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>Your First PHP while loop!</title>
+	</head>
+	<body>
+    <?php
+	$loopCond = true;
+	while ($loopCond == true){
+		//Echo your message that the loop is running below
+		
+		$loopCond = false;
+		echo "<p>The loop is running.</p>";
+	}
+	echo "<p>And now it's done.</p>";
+    ?>
+    </body>
+</html>
---
+++ LOOPING the loop/Exercise3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>A loop of your own</title>
+        <link type='text/css' rel='stylesheet' href='style.css'/>
+	</head>
+	<body>
+    <?php
+	//Add while loop below
+    $a = false;
+	while ($a == true){
+		//Echo your message that the loop is running below
+		
+		$a = false;
+		echo "<p>The loop is running.</p>";
+	}
+	echo "<p>And now it's done.</p>";
+    ?>
+    </body>
